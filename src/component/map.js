c398b29de4d353c2bb142c530098b1f4f1f460d5
benchengfeng <amine_bh@outlook.com>
2022-02-18 16:39:52 +0100

    marker_set

--- src/component/map.js
+++ src/component/map.js
@@ -2,96 +2,92 @@ import Axios from "axios";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import L from "leaflet";
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
 
-  // const redIcon = L.icon({
-  //   iconUrl: "https://assets.stickpng.com/images/580b585b2edbce24c47b2a24.png",
-  //   iconSize: [38, 95], 
-  //   iconAnchor: [22, 94], 
-  //   popupAnchor: [-3, -76] 
-  // });
-  
-  // const blueIcon = L.icon({
-  //   iconUrl: "https://assets.stickpng.com/images/580b585b2edbce24c47b2a24.png",
-  //   iconSize: [38, 95],
-  //   iconAnchor: [22, 94],
-  //   popupAnchor: [-3, -76] 
-  // });
+delete L.Icon.Default.prototype._getIconUrl;
 
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
 
+// const redIcon = L.icon({
+//   iconUrl: "https://assets.stickpng.com/images/580b585b2edbce24c47b2a24.png",
+//   iconSize: [38, 95],
+//   iconAnchor: [22, 94],
+//   popupAnchor: [-3, -76]
+// });
+
+// const blueIcon = L.icon({
+//   iconUrl: "https://assets.stickpng.com/images/580b585b2edbce24c47b2a24.png",
+//   iconSize: [38, 95],
+//   iconAnchor: [22, 94],
+//   popupAnchor: [-3, -76]
+// });
 
 const MapComponent = () => {
-
- 
-
   const lunchState = useSelector((lunchState) => lunchState.LunchState);
   const villageState = useSelector((villageState) => villageState.VillageState);
-  
+
   const [lunch, setLunch] = useState([]);
   const [village, setVillage] = useState([]);
   const [popeye, setPopeye] = useState([]);
-  const [positionVillage, setPositionVillage] = useState([35.915087047076575,14.495279788970945]);
-  const [positionLunch, setPositionLunch] = useState([35.915087047076575,14.495279788970945]);
+  const [positionVillage, setPositionVillage] = useState([
+    35.915087047076575, 14.495279788970945,
+  ]);
+  const [positionLunch, setPositionLunch] = useState([
+    35.915087047076575, 14.495279788970945,
+  ]);
 
   useEffect(() => {
     if (lunchState) setLunch([...lunchState.coordinates]);
   }, [lunchState]);
 
   useEffect(() => {
-    if (villageState?.coordinates.length>0) setVillage(villageState);
+    if (villageState?.coordinates.length > 0) setVillage(villageState);
   }, [villageState]);
 
   useEffect(() => {
-    if (village.coordinates?.length>0) {
-    setPositionVillage (village.coordinates[50])
-    console.log('map position Village', village.coordinates[50])
+    if (village.coordinates?.length > 0) {
+      setPositionVillage(village.coordinates[50]);
+      console.log("map position Village", village.coordinates[50]);
     }
   }, [village]);
 
   useEffect(() => {
-    if (lunch.coordinates?.length>0) {
-    setPositionLunch (lunch.coordinates[50])
-    console.log('map position lunch', lunch.coordinates[50])
+    if (lunch.coordinates?.length > 0) {
+      setPositionLunch(lunch.coordinates[50]);
+      console.log("map position lunch", lunch.coordinates[50]);
     }
   }, [village]);
-
 
   return (
     <>
       <MapContainer
         center={positionVillage}
         zoom={20}
-        style={{ height: "440px" ,marginTop: "80px", marginBottom: "90px" }}
+        style={{ height: "440px", marginTop: "80px", marginBottom: "90px" }}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
-{lunch.coordinates?.map((point,i) => (
-  <Marker
-                key={i}
-                position={[35.915087047076575,14.495279788970945]}
-                // icon={popeye ? redIcon : blueIcon}
-                // zIndexOffset={place.placeClicked ? 10000 : 0}
-                // onClick={e => {
-                //   e.target.setIcon(redIcon);
-                //   setTimeout(() => {
-                //     e.target.setIcon(blueIcon);
-                //   }, 1500);
-                // }}
-              >
-                <Popup>
-                  <p className="place-name">{"[place.venue.name]"}</p>
-                  <p className="place-address">
-                    {"[place.venue.location.address]"}
-                  </p>
-                  <p className="place-category">
-                    {"[place.venue.categories[0].name]"}
-                  </p>
-                </Popup>
-        </Marker>
-))}
+
+          <Marker position={[35.915087047076575, 14.495279788970945]}>
+            <Popup>
+              <p className="place-name">{"[place.venue.name]"}</p>
+              <p className="place-address">
+                {"[place.venue.location.address]"}
+              </p>
+              <p className="place-category">
+                {"[place.venue.categories[0].name]"}
+              </p>
+            </Popup>
+          </Marker>
+
       </MapContainer>
     </>
   );
